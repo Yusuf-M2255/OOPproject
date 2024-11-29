2825132d9a7773f07b12c16334b0ca0610a93a49
hussein145 <140104749+hussein145@users.noreply.github.com>
2024-11-29 19:59:03 +0200

    Update Content.java

--- Watchit/src/ContentControl/Content.java
+++ Watchit/src/ContentControl/Content.java
@@ -1,4 +1,42 @@
 package ContentControl;
 
+import java.awt.*;
+import java.util.Date;
+
 public class Content {
+    public String contentID;
+    public String contentTitle;
+    public Date Date;
+    public CastMember [] cast;
+    public Director director;
+    public String genres;
+    public String language;
+    public String country;
+    public int budget;
+    public int revenue;
+    public Image image;
+    public float [] rating;
+
+    public Content(String contentID, String contentTitle, java.util.Date date, CastMember[] cast, Director director, String genres, String language, String country, int budget, int revenue, Image image, float[] rating) {
+        this.contentID = contentID;
+        this.contentTitle = contentTitle;
+        Date = date;
+        this.cast = cast;
+        this.director = director;
+        this.genres = genres;
+        this.language = language;
+        this.country = country;
+        this.budget = budget;
+        this.revenue = revenue;
+        this.image = image;
+        this.rating = rating;
+    }
+
+    public void RateContent(int contentID, float rate){
+        rating[contentID] = rate;
+    }
+    public float GetRate(int contentID){
+        return rating[contentID];
+    }
 }
+
