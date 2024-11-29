accfcb562523e6bd157c9966659a285aff5029c6
hussein145 <140104749+hussein145@users.noreply.github.com>
2024-11-30 01:32:13 +0200

    Update Content.java

--- Watchit/src/ContentControl/Content.java
+++ Watchit/src/ContentControl/Content.java
@@ -2,11 +2,10 @@ package ContentControl;
 
 import Cast.CastMember;
 import Cast.Director;
-
 import java.awt.*;
 import java.util.Date;
 
-public class Content {
+abstract public class Content {
     public String contentID;
     public String contentTitle;
     public Date Date;
@@ -19,12 +18,16 @@ public class Content {
     public int revenue;
     public Image image;
     public float [] rating;
+    public int RateCounter;
+    public float Rate_Sum;
 
-    public Content(String contentID, String contentTitle, java.util.Date date, CastMember[] cast, Director director, String genres, String language, String country, int budget, int revenue, Image image, float[] rating) {
+    public Content(String contentID, String contentTitle, java.util.Date date, int SizeOfCast, Director director, String genres, String language, String country, int budget, int revenue, Image image) {
+        director.Contents[director.DirectorWorksNumber] = contentTitle;
+        director.DirectorWorksNumber++;
         this.contentID = contentID;
         this.contentTitle = contentTitle;
         Date = date;
-        this.cast = cast;
+        this.cast = new CastMember[SizeOfCast];
         this.director = director;
         this.genres = genres;
         this.language = language;
@@ -32,14 +35,25 @@ public class Content {
         this.budget = budget;
         this.revenue = revenue;
         this.image = image;
-        this.rating = rating;
+        this.rating = new float[5000];
+        Rate_Sum = 0;
     }
 
-    public void RateContent(int contentID, float rate){
-        rating[contentID] = rate;
+    public void AddRate(int UserID, float rate){
+        rating[UserID] = rate;
+        if(rating[UserID] == 0)
+            RateCounter++;
     }
-    public float GetRate(int contentID){
-        return rating[contentID];
+
+    public void RemoveRate(int UserID){
+        Rate_Sum -= rating[UserID];
+        RateCounter--;
+        rating[UserID] = 0;
     }
+    public float GetRate(){
+        return (Rate_Sum/RateCounter);
+    }
+    abstract void AddCast();
+
 }
 
