158256ddf5d254e3ef04948f93dc085e177aa5eb
hussein145 <140104749+hussein145@users.noreply.github.com>
2024-11-29 20:00:13 +0200

    Update Director.java

--- Watchit/src/Cast/Director.java
+++ Watchit/src/Cast/Director.java
@@ -1,4 +1,23 @@
 package Cast;
 
+import java.util.Date;
+
 public class Director {
+    public String firstName;
+    public String lastName;
+    public Date dateOfBirth;
+    public String gender;
+    public Movie [] movies;
+    public String nationality;
+    public String socialMediaLink;
+
+    public Director(String firstName, String lastName, Date dateOfBirth, String gender, Movie[] movies, String nationality, String socialMediaLink) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.movies = movies;
+        this.nationality = nationality;
+        this.socialMediaLink = socialMediaLink;
+    }
 }
