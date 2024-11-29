c6570780d3f963324e12ab04c0cbc8a90023e6ae
IbrahimYasserM <2023170016@cis.asu.edu.eg>
2024-11-29 16:34:18 +0200

    adding movie class basic structure

--- Watchit/src/ContentControl/Episode.java
+++ Watchit/src/ContentControl/Episode.java
@@ -1,4 +1,32 @@
 package ContentControl;
 
 public class Episode {
+  // Variables
+  private int episodeNumber;
+  private String episodeTitle;
+  private int duration;
+  private Date releaseDate;
+
+  // Constructors
+  public Episode(int episodeNumber, String episodeTitle, int duration, Date releaseDate) {
+    this.episodeNumber = episodeNumber;
+    this.episodeTitle = episodeTitle;
+    this.duration = duration;
+    this.releaseDate = releaseDate;
+  }
+
+  // getters
+  public int getepisodeNumber(){
+    return this.episodeNumber;
+  }
+  public int getepisodeTitle(){
+    return this.episodeTitle;
+  }
+  public int getduration(){
+    return this.duration;
+  }
+  public int getreleaseDate(){
+    return this.releaseDate;
+  }
+
 }
