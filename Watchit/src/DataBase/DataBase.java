312e510a40306dc3f9771e920d11fe214d721f64
hossames <100873739+hossames@users.noreply.github.com>
2024-11-29 22:00:13 +0200

    src. deleted ya 3ly

--- Watchit/src/DataBase/DataBase.java
+++ Watchit/src/DataBase/DataBase.java
@@ -1,27 +1,19 @@
 package DataBase;
 
 import AccountControl.*;
-import Cast.CastMember;
-import Cast.Director;
 import ContentControl.*;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
 
 public class DataBase {
     private static DataBase dataBase;
     public AdminsData adminsData= null;
-    private List<Account> accounts = null;
-    private MoviesData moviesData = null;
+    public List<Account> accounts = null;
+    public MoviesData moviesData = null;
     public UsersData usersData = null;
     public WatchRecordData watchRecordData = null;
-    private SeriesData seriesData = null;
-    private List<Content> contents = null;
+    public SeriesData seriesData = null;
+    public List<Content> contents = null;
     public DirctorsData DirectorsData = null;
     public CastMembersData castMembersData = null;
     private DataBase() {
