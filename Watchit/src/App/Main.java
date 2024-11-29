6a4a32f0a95362e69b060fac9bd6dc59da88a84c
hossames <100873739+hossames@users.noreply.github.com>
2024-11-30 00:11:23 +0200

    main Reset

--- Watchit/src/App/Main.java
+++ Watchit/src/App/Main.java
@@ -4,7 +4,5 @@ import DataBase.DataBase;
 
 public class Main {
     public static void main(String[] args) {
-        DataBase.getInstance().Register();
-        DataBase.getInstance().usersData.SaveData();
     }
 }
