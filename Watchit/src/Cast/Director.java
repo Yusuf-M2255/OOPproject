df92a5caf7b19b09552ebe8e75e3356064b7001a
hussein145 <140104749+hussein145@users.noreply.github.com>
2024-11-30 12:25:58 +0200

    Update Director.java

--- Watchit/src/Cast/Director.java
+++ Watchit/src/Cast/Director.java
@@ -1,27 +1,11 @@
 package Cast;
 
-import java.util.ArrayList;
 import java.util.Date;
-import ContentControl.Content;
+import java.util.List;
 
-public class Director {
-    public String firstName;
-    public String lastName;
-    public Date dateOfBirth;
-    public String gender;
-    public int DirectorWorksNumber;
-    public ArrayList<String> Contents;
-    public String nationality;
-    public String socialMediaLink;
+public class Director extends CastMember{
 
-    public Director(String firstName, String lastName, Date dateOfBirth, String gender, String nationality, String socialMediaLink, int DirectorWorksNumber) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.dateOfBirth = dateOfBirth;
-        this.gender = gender;
-        Contents = new ArrayList<>();
-        this.nationality = nationality;
-        this.socialMediaLink = socialMediaLink;
-        this.DirectorWorksNumber = DirectorWorksNumber;
+    public Director(String firstName, String lastName, Date dateOfBirth, String gender, List<String> Contents, String nationality, String socialMediaLink) {
+        super(firstName, lastName, dateOfBirth, gender, Contents, nationality, socialMediaLink);
     }
 }
