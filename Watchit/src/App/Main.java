5f7d8a20356ef8970a2d6c55f88ccc84359dabf3
yusuf <yusufmahmud2005@gmail.com>
2024-11-30 22:44:24 +0200

    add tmp class

--- Watchit/src/App/Main.java
+++ Watchit/src/App/Main.java
@@ -3,10 +3,15 @@ package App;
 
 import DataBase.DataBase;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         DataBase.getInstance().Register();
         DataBase.getInstance().Save();
+        System.out.println("                                                                                            Welcome To WatchIt Lite");
+        tmp t = new tmp();
+        t.LoginDisplay();
     }
 }
---
+++ Watchit/src/App/tmp.java
@@ -0,0 +1,28 @@
+package App;
+import DataBase.DataBase;
+import java.util.Scanner;
+
+public class tmp {
+    public void LoginDisplay()
+    {
+        Scanner input = new Scanner(System.in);
+        char c;
+        boolean check = false;
+        do {
+            do {
+                System.out.print("Do you have an account? \n (Y/N) ");
+                c = input.next().charAt(0);
+                if (c == 'Y' || c == 'y' || c == 'N' || c == 'n')
+                    break;
+                else
+                    System.out.println("Invalid input, Please try again");
+            }while (true);
+            if (c == 'Y' || c == 'y')
+                check = DataBase.getInstance().Login();
+            else
+                check = DataBase.getInstance().Register();
+            if (check)
+                break;
+        }while (true);
+    }
+}
