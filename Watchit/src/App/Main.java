3044ed0df1156cdaa430c10bf6e6f7a8d3c0058c
yusuf <yusufmahmud2005@gmail.com>
2024-11-30 23:01:39 +0200

    Login Demo V2

--- Watchit/src/App/Main.java
+++ Watchit/src/App/Main.java
@@ -7,11 +7,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        DataBase.getInstance().Register();
-        DataBase.getInstance().Save();
         System.out.println("                                                                                            Welcome To WatchIt Lite");
         tmp t = new tmp();
         t.LoginDisplay();
+        DataBase.getInstance().Save();
     }
 }
