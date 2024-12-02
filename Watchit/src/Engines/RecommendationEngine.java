c51627330104c421890c5ea76803c786184ec55d
Ali-Hady <68974591+Ali-Hady@users.noreply.github.com>
2024-12-02 20:36:17 +0200

    recommendation probably done (hopefully)

--- Watchit/src/Engines/RecommendationEngine.java
+++ Watchit/src/Engines/RecommendationEngine.java
@@ -4,20 +4,49 @@ import ContentControl.Content;
 import AccountControl.*;
 import ContentControl.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RecommendationEngine {
-    private User user;
+    private List<String> favouriteGenres;
     private Content[] content;
 
-    RecommendationEngine(User user, Content[] content) {
-        this.user = user;
+    RecommendationEngine(List<String> favouriteGenres, Content[] content) {
+        this.favouriteGenres = favouriteGenres;
         this.content = content;
     }
 
-    public Content[] getContentBasedRecommendations() {
-        return content;
+    public List<Movie> getMovieBasedRecommendations() {
+        List<Movie> movieBasedRecommendations = new ArrayList<>();
+        for (int i = 0; i < content.length; i++) {
+            if (content[i] instanceof Movie) {
+                Movie movie = (Movie) content[i];
+                for (int j = 0; j < favouriteGenres.size(); j++) {
+                    if (movie.genres.contains(favouriteGenres.get(j))) {
+                        movieBasedRecommendations.add(movie);
+                    }
+                }
+            }
+        }
+
+        return movieBasedRecommendations;
     }
 
-    public Content[] getTrendingRecommendations() {
-        return content;
+    public List<Series> getSeriesBasedRecommendations() {
+        List<Series> seriesBasedRecommendations = new ArrayList<>();
+        for (int i = 0; i < content.length; i++) {
+            if (content[i] instanceof Series) {
+                Series series = (Series) content[i];
+                for (int j = 0; j < favouriteGenres.size(); j++) {
+                    if (series.genres.contains(favouriteGenres.get(j))) {
+                        seriesBasedRecommendations.add(series);
+                    }
+                }
+            }
+        }
+
+        return seriesBasedRecommendations;
     }
+
+    
 }
