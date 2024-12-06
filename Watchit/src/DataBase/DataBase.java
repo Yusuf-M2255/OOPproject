3f495227c71431e7a6b266b733e9cccc3ed06384
hossames <100873739+hossames@users.noreply.github.com>
2024-12-06 15:04:26 +0200

    ok

--- Watchit/src/DataBase/DataBase.java
+++ Watchit/src/DataBase/DataBase.java
@@ -240,12 +240,12 @@ public class DataBase {
             System.out.print("Enter your credit card number: ");
             CreditNumber = sc.nextLine();
             System.out.print("Enter your credit card expiration date (YY/MM): ");
+            Calendar cal = Calendar.getInstance();
             try {
-                Calendar cal = Calendar.getInstance();
                 String[] YYMM = sc.nextLine().split("/");
                 cal.set(Integer.parseInt(YYMM[0]), Integer.parseInt(YYMM[1]), 0);
             }catch (NumberFormatException e){
-                e.printStackTrace();;
+                System.out.println("Error Enter Number plz");
             }
             System.out.print("Enter your credit card CVV: ");
             cvv = sc.nextLine();
